840422e012b87348555ca83e66834a245ff36166
egoksg <egoksg@gmail.com>
2019-01-28 22:26:55 +0900

    content 예제 작성 resources\static\index.html를 참조하여 작성되었다.

---
+++ src/main/java/com/hmi/web/ExapmleController.java
@@ -0,0 +1,17 @@
+package com.hmi.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+public class ExapmleController {
+
+    @GetMapping("/layout_content_example")
+    public String main() {
+        return "view/layout_content_example";
+    }
+    
+}
