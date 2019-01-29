e0098b3bd48f7f368117ae9af7a18b095c588fa0
egoksg <egoksg@gmail.com>
2019-01-29 22:45:10 +0900

    JPA 예제

--- src/main/java/com/hmi/web/ExapmleController.java
+++ src/main/java/com/hmi/web/ExapmleController.java
@@ -2,16 +2,28 @@ package com.hmi.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.hmi.dto.HmiRoomaSaveRequestDto;
+import com.hmi.service.HmiControlRecordService;
 
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Controller
 public class ExapmleController {
+	
+	private HmiControlRecordService hmiRoomaService;
 
     @GetMapping("/layout_content_example")
     public String main() {
         return "view/layout_content_example";
+    }
+    
+    @PutMapping("/saveRooma")
+    public void saveRooma(@RequestBody HmiRoomaSaveRequestDto dto){
+    	hmiRoomaService.save(dto);
     }
     
 }
